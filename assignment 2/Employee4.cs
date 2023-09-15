312fb8ba94cc78fd270d1bc5756aecfbdb6cc24c
govindsinghgs <129824672+govindsinghgs@users.noreply.github.com>
2023-09-14 20:26:59 -0700

    Update Employee4.cs

--- assignment 2/Employee4.cs
+++ assignment 2/Employee4.cs
@@ -11,7 +11,7 @@ namespace CsharpPrograms
         /*static void Main(string[] args)
         {
             Employee2 obj4 = new Employee2();
-            obj4.read("Akash singh", "akashsisodiya666@gmail.com", +918171078563);
+            obj4.read("Akash singh", "govindsingh66@gmail.com", +918171078563);
             obj4.read(50000, 20000, 15000);
             obj4.display();
         }*/
